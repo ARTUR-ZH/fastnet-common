a650a3825af8bbd54b0b012f07f9e9a6d8ac961c
asimshah <asimshah@hotmail.com>
2015-04-11 14:47:27 +0530

    First upload of Fastnet libraries - only contains Fastnet.Common at this point

---
+++ Fastnet.Common/ApplicationSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Fastnet.Common
+{
+    public static class ApplicationSettings
+    {
+
+        public static T Key<T>(string name, T defaultValue)
+        {
+            string valueSetting = ConfigurationManager.AppSettings[name];
+            if (valueSetting == null)
+            {
+                return defaultValue;
+            }
+            return TConverter.ChangeType<T>(valueSetting);
+        }
+    }
+}
